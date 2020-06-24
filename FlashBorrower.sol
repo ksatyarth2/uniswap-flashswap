ed67e766db47197179e487f495e74e832b2d06c8
Austin Williams <Austin-Williams@users.noreply.github.com>
2020-06-23 18:00:41 -0600

    tighten up access controls

--- FlashBorrower.sol
+++ FlashBorrower.sol
@@ -9,7 +9,7 @@ contract UniswapV2FlashBorrower {
     address constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
     address constant DAI = 0x6B175474E89094C44Da98b954EedeAC495271d0F;
     
-    bool unlocked; // access control
+    address pairAddress; // access control
     
     // TESTING
     uint public lastAmount; // just for testing
@@ -20,16 +20,14 @@ contract UniswapV2FlashBorrower {
     // @param _amount The amount of _token you will borrow
     function flashLoan(address _token, address _tokenB, uint256 _amount) external {
         // address tokenB = _token == WETH ? DAI : WETH;
-        address pairAddress = uniswapV2Factory.getPair(_token, _tokenB); // is it cheaper to compute this locally?
+        pairAddress = uniswapV2Factory.getPair(_token, _tokenB); // is it cheaper to compute this locally?
         require(pairAddress != address(0), "Requested _token is not available.");
         address token0 = IUniswapV2Pair(pairAddress).token0();
         address token1 = IUniswapV2Pair(pairAddress).token1();
         uint amount0Out = _token == token0 ? _amount : 0;
         uint amount1Out = _token == token1 ? _amount : 0;
         bytes memory data = abi.encode(_token, _amount);
-        unlocked = true; // allows pair to call `uniswapV2Call` on our contract
         IUniswapV2Pair(pairAddress).swap(amount0Out, amount1Out, address(this), data);
-        unlocked = false; // ensures nobody else can call `uniswapV2Call` on our contract
     }
     
     
@@ -37,7 +35,8 @@ contract UniswapV2FlashBorrower {
     // @notice Function is called by the Uniswap V2 pair's `swap` function, passing in the `data` we defined above
     function uniswapV2Call(address sender, uint amount0, uint amount1, bytes calldata data) external {
         // access control
-        require(unlocked, "function can be initiated by our flashLoan only");
+        require(msg.sender == pairAddress, "only UniswapV2 pair can call");
+        require(sender == address(this), "only this contract may initiate");
         
         // decode data
         (address _token, uint _amount) = abi.decode(data, (address, uint));
