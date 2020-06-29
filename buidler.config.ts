58577403f966e8f8162e74566f6bb847e9a7e7dd
ajs <palebluedot@gmail.com>
2020-06-29 12:21:22 -0300

    Start buidler bootstrapping

---
+++ buidler.config.ts
@@ -0,0 +1,12 @@
+import { BuidlerConfig } from '@nomiclabs/buidler/config'
+
+const config: BuidlerConfig = {
+  solc: {
+    version: "0.5.17"
+  },
+  paths: {
+    artifacts: './artifacts'
+  }
+}
+
+export default config
